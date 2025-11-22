3fea4893036a0df024e91747fefb1a1148467906
fullstack635 <fullstack530.dev@gmail.com>
2025-11-22 15:11:08 -0800

    updated mobile responsive

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -18,7 +18,7 @@ function App() {
     <QueryProvider>
       <AppProvider>
         <TooltipProvider>
-          <ThemeSwitcher />
+          <ThemeSwitcher className="hidden" />
           <Toaster />
           <Sonner />
           <Router>
